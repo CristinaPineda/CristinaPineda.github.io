eb75664960e883423072992baee9ee2cf93fcc92
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-09-16 21:43:38 -0300

    header e componentes auxiliares

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import cris from '../images/cris.jpeg';
+import Skills from './Skills';
+import '../styles/header.css';
+
+export default function Header() {
+  return(
+    <header className="header">
+      <img src={ cris } alt="fots Cristina" width="150rem" id="myPhoto" />
+      <h1>
+        Cristina Pineda Web Developer />
+      </h1>
+      <Skills />
+    </header>
+  )
+}
