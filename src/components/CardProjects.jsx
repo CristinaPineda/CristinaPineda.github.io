6ada0a85057c2ec7409e1cd0a7edcde86ba4a424
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-02 19:05:34 -0300

    dois projetos inclusos

--- src/components/CardProjects.jsx
+++ src/components/CardProjects.jsx
@@ -6,32 +6,18 @@ export default function CardProjects() {
   return(
       <div className="projects">
         <div className="card-project">
-          <h3>To do list</h3>
-          <p>Crie listas de forma dinamica</p>
-          <p>Acesse a aplicação</p>
-          <p>Link do repositório</p>
-            <img src={ todo } alt="todo" width="250px" />
+          <h2>Pixels Art</h2>
+          <p>Divirta-se criando desenhos ponto a ponto</p>
+          <a href="https://cristinapineda.github.io/pixelsArt/" target="blank">Acesse a aplicação</a>
+          <a href="https://github.com/CristinaPineda/pixelsArt" target="blank">Link do repositório</a>
+          <img src={ pixel } alt="todo" width="250px" />
         </div>
         <div className="card-project">
-          <h3>To do list</h3>
+          <h2>To do list</h2>
           <p>Crie listas de forma dinamica</p>
-          <p>Acesse a aplicação</p>
-          <p>Link do repositório</p>
-            <img src={ pixel } alt="todo" width="250px" />
-        </div>
-        <div className="card-project">
-          <h3>To do list</h3>
-          <p>Crie listas de forma dinamica</p>
-          <p>Acesse a aplicação</p>
-          <p>Link do repositório</p>
-            <img src={ pixel } alt="todo" width="250px" />
-        </div>
-        <div className="card-project">
-          <h3>To do list</h3>
-          <p>Crie listas de forma dinamica</p>
-          <p>Acesse a aplicação</p>
-          <p>Link do repositório</p>
-            <img src={ pixel } alt="todo" width="250px" />
+          <a href="https://cristinapineda.github.io/to-do-list-redux/" target="blank">Acesse a aplicação</a>
+          <a href="https://github.com/CristinaPineda/to-do-list-redux" target="blank">Link do repositório</a>
+          <img src={ todo } alt="todo" width="250px" height="210px" />
         </div>
       </div>
   )
