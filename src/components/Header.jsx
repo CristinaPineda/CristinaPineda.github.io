9bb12615ab340e882c2958f48c210676f6c82dba
CristinaPineda <cristinadaspineda@yahoo.com.br>
2021-10-10 20:24:10 -0300

    media header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
   return (
     <header className="header">
       <div className="image">
-        <img src={ cris } alt="fots Cristina" width="220em" id="my-photo" />
+        <img src={ cris } alt="foto Cristina" width="220em" id="my-photo" />
         <h1>
           Cristina Pineda Web Developer /{'>'}
         </h1>
